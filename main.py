e1deadbff64bc7d821cb6791d3b3bdacbbe61f6d
raunaks13 <foleys.raunak@gmail.com>
2018-03-13 18:49:55 +0530

    added compatibility with flask

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+#@app.route('/')
+#def hello_world():
+#  return 'Bansuri Makhanchorwale'
+
 @app.route('/')
-def hello_world():
-  return 'Bansuri Makhanchorwale'
+def home():
+  return render_template('index.html')
+
+#@app.route('/getconfig', methods=['GET', 'POST'])
+#def config():
+#    value = request.form['config']
+#    print ("This is the user value :  ", value);
 
 if __name__ == '__main__':
   app.run()
